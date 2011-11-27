39dade8b526415a266870504e1ec917ed6408812
mahesheu <mahesheu@gmail.com>
2011-11-28 04:47:11 +0530

    'firstcommit'

---
+++ js/slide.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+			
+		$("#mobile").click(function()
+		{
+	
+			$.scrollTo("#slide1",800,{easing:'easeInOutCirc'})
+		});		
+		$("#design").click(function()
+		{
+			$.scrollTo("#slide2",800,{easing:'easeInOutCirc'})
+		});
+		$("#framework").click(function()
+		{
+			$.scrollTo("#slide3",800,{easing:'easeInOutCirc'})
+		});
+		$("#cloud").click(function()
+		{
+			$.scrollTo("#slide4",800,{easing:'easeInOutCirc'})
+		});
+		$("#electronics").click(function()
+		{
+			$.scrollTo("#slide5",800,{easing:'easeInOutCirc'})
+		});
+	});
